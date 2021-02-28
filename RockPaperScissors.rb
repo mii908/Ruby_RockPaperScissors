6159c0542089bfa32599691102eeb0d3a7f80ddf
EC2 Default User <ec2-user@ip-172-31-7-156.us-east-2.compute.internal>
2021-02-28 14:08:59 +0000

    課題（じゃんけんアプリ）再提出

--- RockPaperScissors.rb
+++ RockPaperScissors.rb
@@ -13,7 +13,7 @@ def battle
             puts "あなた：パーを出しました"
         else
             puts "戦いません"
-            sleep
+            exit
         end
     end
     
@@ -37,7 +37,40 @@ def battle
     strategy1(myChoice)
     opponentStrategy1(opponentChoice)
     puts "------------------------"
-    
+
+    def judge(m, o)
+        case
+        when m == "0" && o == "1"
+            $winner = "あなた"
+            $looser = "相手"
+        when m == "1" && o == "2"
+            $winner = "あなた"
+            $looser = "相手"
+        when m == "2" && o == "0"
+            $winner = "あなた"
+            $looser = "相手"
+        when o == "0" && m == "1"
+            $winner = "相手"
+            $looser = "あなた"
+        when o == "1" && m == "2"
+            $winner = "相手"
+            $looser = "あなた"
+        when o == "2" && m == "0"
+            $winner = "相手"
+            $looser = "あなた"
+        when m == o
+            $winner = 0
+            $looser = 0
+        end
+    end
+
+    judge(myChoice, opponentChoice)
+    unless $winner == $looser
+        puts "じゃんけんの勝ち:#{$winner}"
+        puts "じゃんけんの負け:#{$looser}"
+        puts "------------------------"
+    end
+
     if myChoice == opponentChoice
         puts "あいこで..."
         puts "0(グー)1(チョキ)2(パー)3(戦わない)"
@@ -45,31 +78,51 @@ def battle
         battle
     end
 end
-
-def strategy2(choice)
-    if choice == "0"
-            puts "あなた：上"
-        elsif choice == "1"
-            puts "あなた：下"
-        elsif choice == "2"
-            puts "あなた：左"
-        elsif choice == "3"
-            puts "あなた：右"
-        else
-            puts "戦いません"
+def battle2
+    def strategy2(myChoice2)
+        if myChoice2 == "0"
+                puts "あなた：上"
+            elsif myChoice2 == "1"
+                puts "あなた：下"
+            elsif myChoice2 == "2"
+                puts "あなた：左"
+            elsif myChoice2 == "3"
+                puts "あなた：右"
+            else
+                puts "戦いません"
+        end
     end
-end
 
-def opponentStrategy2
+    def opponentStrategy2(opponentChoice2)
+        if opponentChoice2 == "0"
+            puts "相手：上"
+        elsif opponentChoice2 == "1"
+            puts "相手：下"
+        elsif opponentChoice2 == "2"
+            puts "相手：左"
+        elsif opponentChoice2 == "3"
+            puts "相手：右"
+        end
+
+    end
+
+    myChoice2 = gets.chomp
     opponentChoice2 = rand(0..3).to_s
-    if opponentChoice2 == "0"
-        puts "相手：上"
-    elsif opponentChoice2 == "1"
-        puts "相手：下"
-    elsif opponentChoice2 == "2"
-        puts "相手：左"
-    elsif opponentChoice2 == "3"
-        puts "相手：右"
+    puts "ホイ！"
+    puts "------------------------"
+
+    strategy2(myChoice2)
+    opponentStrategy2(opponentChoice2)
+
+    if $winner == "あなた" && myChoice2 == opponentChoice2
+        puts "あなたの勝ちです"
+        puts "------------------------"
+    elsif $looser == "あなた" && myChoice2 == opponentChoice2
+        puts "あなたの負けです"
+        puts "------------------------"
+    else
+        puts "じゃんけんからやり直し"
+        puts "------------------------"
     end
 end
 
@@ -80,10 +133,5 @@ loop do
     
     puts "あっち向いて〜"
     puts "0(上)1(下)2(左)3(右)"
-    myChoice2 = gets.chomp
-    puts "ホイ！"
-    puts "------------------------"
-    
-    strategy2(myChoice2)
-    opponentStrategy2
+    battle2
 end
